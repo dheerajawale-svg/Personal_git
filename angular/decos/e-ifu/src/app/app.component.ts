9e3994a5929907ac4f4a17ada3e37aa7373b3bf3
Dheeraj-Awale_natus <Dheeraj.Awale@natus.com>
2023-09-05 22:57:11 +0530

    form validations and colors

--- angular/decos/e-ifu/src/app/app.component.ts
+++ angular/decos/e-ifu/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, NgForm, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatRadioChange } from '@angular/material/radio';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'e-ifu';
@@ -14,12 +16,16 @@ export class AppComponent {
 
   consentForm: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(private _formBuilder: FormBuilder, private _snackBar: MatSnackBar) {
     this.consentForm = _formBuilder.group({
-      email: [this.email, Validators.required],
+      email: [this.email],
       purpose:['', Validators.required],
       checked: new FormControl(false)
     })
+  }
+
+  ngOnInit() {
+    this.boxChanged(false);
   }
 
   radioSelected(event: MatRadioChange) {
@@ -28,8 +34,39 @@ export class AppComponent {
     // console.log(this.radioValue);
   }
 
+  boxChanged(evchecked: any) {
+    console.log(evchecked);
+    if(!evchecked) {
+      this.consentForm.controls["email"].setValidators([Validators.required, Validators.email]);
+    }
+    else {
+      this.consentForm.controls["email"].setValidators(null);
+    }
+    this.consentForm.controls["email"].updateValueAndValidity();
+  }
+
   onNgSubmit() {
+    if(this.consentForm.invalid) {
+      this.openSnackbar("Invalid Data!!!");
+    }
+
     console.log(this.consentForm?.value);
     console.log(this.radioValue);
+  }
+
+  openSnackbar(message: string) {
+    let snackBarRef = this._snackBar.open(message, 'Close', {
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      duration: 3000
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      console.log('The snackbar action was triggered!');
+    });
+
+    snackBarRef.afterDismissed().subscribe(() => {
+      console.log('The snackbar was dismissed');
+    });
   }
 }

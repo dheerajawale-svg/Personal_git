adae2bc7c92d80c82ee38a4056421321360cdb7e
Dheeraj-Awale_natus <Dheeraj.Awale@natus.com>
2023-09-20 17:02:07 +0530

    adding hover effects

--- angular/decos/e-ifu/src/app/card-content/card-content.component.ts
+++ angular/decos/e-ifu/src/app/card-content/card-content.component.ts
@@ -8,4 +8,26 @@ import { Component } from '@angular/core';
 export class CardContentComponent {
   selected = 'option2';
   panelOpenState = false;
+  favIcon = ['material-icons-outlined'];
+  favSelected = false;
+  // <div ng-class="[{true: 'material-icons favColor-active'}[true]]"></div>
+
+
+  favIconClicked() {
+    this.favSelected = !this.favSelected;
+    if(this.favSelected) {
+      this.favIcon = ['material-icons', 'favColor-active']
+    }
+    else {
+      this.favIcon = ['material-icons-outlined']
+    }
+
+    // if(this.favIcon == "material-icons-outlined") {
+    //   this.favIcon = "material-icons";
+    //   console.log("Clicked!!!!!!")
+    // }
+    // else {
+    //   this.favIcon = "material-icons-outlined";
+    // }
+  }
 }

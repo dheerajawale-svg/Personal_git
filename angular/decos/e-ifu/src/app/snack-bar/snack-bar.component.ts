7830fe3faa214071c3e70453d291fec797db5ac0
Dheeraj-Awale_natus <Dheeraj.Awale@natus.com>
2023-09-27 20:30:51 +0530

    fixed progress bar animation

--- angular/decos/e-ifu/src/app/snack-bar/snack-bar.component.ts
+++ angular/decos/e-ifu/src/app/snack-bar/snack-bar.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, Inject, ViewChild, inject } from '@angular/core';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
-import { delay, interval, map, startWith } from 'rxjs';
+import { delay, interval, map, startWith, tap, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-snack-bar',
@@ -13,7 +13,7 @@ export class SnackBarComponent implements AfterViewInit {
 
   @ViewChild(MatProgressBar) progressBar!: MatProgressBar;
 
-  constructor(@Inject(MAT_SNACK_BAR_DATA) public data:any) {
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) {
 
   }
 
@@ -21,24 +21,49 @@ export class SnackBarComponent implements AfterViewInit {
 
   startTimer(seconds: number) {
     const intervalPeriod = 60;
-    const timer$ = interval(intervalPeriod);
 
-    const sub = timer$.subscribe((sec) => {
-      this.value = 100 - sec * 100 / seconds;
-      this.curSec = sec;
+    const sub = interval(intervalPeriod)
+      .pipe(
+        // throttle for animation
+        throttleTime(100),
+        // business logic with event
+        tap(val => {
+          this.value = 100 - val * 100 / seconds;
+          this.curSec = val;
 
-      // console.log('value: %d', this.value);
-      // console.log('curSec: %d', this.curSec);
+          if (this.curSec === intervalPeriod) {
+            this.data.snackBar.dismiss();
+            sub.unsubscribe();
+          }
+        })
+      )
+      .subscribe();
 
-      if (this.curSec === intervalPeriod) {
-        sub.unsubscribe();
-        this.data.snackBar.dismiss();
-      }
-    });
+
+    // const timer$ = interval(intervalPeriod);
+
+    // const sub = timer$.subscribe((sec) => {
+    //   this.value = 100 - sec * 100 / seconds;
+    //   this.curSec = sec;
+
+    //   // console.log('value: %d', this.value);
+    //   // console.log('curSec: %d', this.curSec);
+
+    //   if (this.curSec === intervalPeriod) {
+    //     sub.unsubscribe();
+    //     this.data.snackBar.dismiss();
+    //   }
+    // });
   }
 
   ngAfterViewInit() {
-    this.startTimer(55);
+
+    setTimeout(() => {
+      this.startTimer(55);
+      // this.value = 0;
+    }, 500);
+
+
   }
 
   onAction() {

f8ce9e53fc3260962408afd1b087e39a4a0ba80b
Dheeraj-Awale_natus <Dheeraj.Awale@natus.com>
2024-01-05 14:46:26 +0530

    used signals to notify UI refresh

--- angular/decos/e-ifu/src/app/app.module.ts
+++ angular/decos/e-ifu/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -13,15 +12,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatDatepickerModule } from "@angular/material/datepicker";
-import {  MatDialogModule } from "@angular/material/dialog";
-import {  MatInputModule } from "@angular/material/input";
-import {  MatListModule } from "@angular/material/list";
-import {  MatPaginatorModule } from "@angular/material/paginator";
-import {  MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import {  MatSelectModule } from "@angular/material/select";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatSortModule } from "@angular/material/sort";
-import {  MatTableModule } from "@angular/material/table";
+import { MatTableModule } from "@angular/material/table";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
@@ -29,15 +28,15 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatTooltipModule} from '@angular/material/tooltip';
 import { MatStepperModule } from '@angular/material/stepper';
-import {MatGridListModule } from '@angular/material/grid-list';
-import {MatTreeModule } from '@angular/material/tree';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTreeModule } from '@angular/material/tree';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatExpansionModule} from '@angular/material/expansion';
 import { MatRippleModule } from '@angular/material/core';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressBarModule} from '@angular/material/progress-bar';
 
 import { AppComponent } from './app.component';
 import { ThemeSwitchComponent } from './theme-switch/theme-switch.component';
